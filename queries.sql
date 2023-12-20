d22ba80ec5a7b1cbb323b4ec7724be1b57106d59
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-20 13:55:57 +0100

    add: third task

--- queries.sql
+++ queries.sql
@@ -5,10 +5,19 @@ FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990
 ORDER BY `date_of_birth` ASC
 
+
 -- Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
 SELECT `name`, `cfu`
 FROM `courses`
 WHERE `cfu` > 10
 ORDER BY `cfu` ASC
+
+
+-- Selezionare tutti gli studenti che hanno più di 30 anni
+
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365.25 > 30 -- il .25 l'ho messo per contare gli anni bisestili, altrimenti mi contava i giorni dei neo-trentenni dal 26 dicembre 1993
+ORDER BY `date_of_birth` DESC;
 
