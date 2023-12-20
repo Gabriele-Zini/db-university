4583c6ab29e223485c0dddebc8b3d252bb186e0e
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-20 13:41:35 +0100

    add:  second task

--- queries.sql
+++ queries.sql
@@ -4,3 +4,11 @@ SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990
 ORDER BY `date_of_birth` ASC
+
+-- Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+SELECT `name`, `cfu`
+FROM `courses`
+WHERE `cfu` > 10
+ORDER BY `cfu` ASC
+
