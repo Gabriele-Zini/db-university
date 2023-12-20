b791d8cedac7ea32c7a776cee92a9a183f02d299
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-20 14:21:24 +0100

    add: fifth task

--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ WHERE YEAR(`date_of_birth`) = 1990
 ORDER BY `date_of_birth` ASC
 
 
--- Selezionare tutti i corsi che valgono più di 10 crediti (479)
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
 SELECT `name`, `cfu`
 FROM `courses`
@@ -14,14 +14,14 @@ WHERE `cfu` > 10
 ORDER BY `cfu` ASC
 
 
--- Selezionare tutti gli studenti che hanno più di 30 anni
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365.25 > 30 -- il .25 l'ho messo per contare gli anni bisestili, altrimenti mi contava i giorni dei neo-trentenni dal 26 dicembre 1993
 ORDER BY `date_of_birth` DESC;
 
--- questa variante è più precisa e mostra anche gli students che hanno compiuto 30 anni nel giorno corrente
+-- 3.1 questa variante è più precisa e mostra anche gli students che hanno compiuto 30 anni nel giorno corrente
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
@@ -30,8 +30,16 @@ ORDER BY `date_of_birth` DESC;
 
 
 
--- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 SELECT `period`, `year`
 FROM `courses`
 WHERE `period`= 'I semestre' AND `year`='1'
+
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT `hour`
+FROM `exams`
+WHERE `hour` >= '14:00' AND `date` = '2020/06/20'
+ORDER BY `hour` ASC;
