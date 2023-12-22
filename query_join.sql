95ef64d2a4bad06298c9c440d8d515d2d4d1220a
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2023-12-22 10:08:15 +0100

    fix: degrees_name

--- query_join.sql
+++ query_join.sql
@@ -1,6 +1,6 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
-SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` 
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS `degrees_name`
 FROM `students`
 INNER JOIN `degrees`
 ON `students`.`degree_id`=`degrees`.`id`
